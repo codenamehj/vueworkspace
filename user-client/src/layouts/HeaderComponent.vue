b61002eadd1d6d022f99e5ad9a532e630b7fabc1
admin <admin@DESKTOP-9K6N0AO>
2024-02-05 17:12:42 +0900

    user-client

---
+++ user-client/src/layouts/HeaderComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/userList">전체 조회</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="#">등록</router-link>
+        </li>
+        <li class="nav-item">
+            <router-link class="nav-link" to="#">Link</router-link>
+        </li>
+    </ul>
+</template>
---
+++ user-client/src/views/UserListView.vue
@@ -0,0 +1,17 @@
+<!-- UserListView.vue -->
+<template>
+  <div class="container">
+    <h1>전체 회원 조회</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  methods: {
+    getUserList () {
+      console.log('Ajax로 데이터 조회')
+    }
+  }
+}
+</script>
